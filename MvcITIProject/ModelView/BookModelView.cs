08a1190f889988a6fc96562b543683bd6a1423b0
3omda <3omda@iti>
2025-06-28 16:49:22 +0300

    Modelview + bookcontroller

---
+++ MvcITIProject/ModelView/BookModelView.cs
@@ -0,0 +1,24 @@
+﻿using MvcITIProject.Models;
+
+namespace MvcITIProject.ModelView
+{
+    public class BookModelView
+    {
+        public string Title { get; set; } = null!;
+
+        public int? CatId { get; set; }
+
+        public int? PublisherId { get; set; }
+
+        public string? ShelfCode { get; set; } = null!;
+
+
+        public virtual Category Cat { get; set; } = null!;
+
+        public virtual Publisher Publisher { get; set; } = null!;
+
+        public virtual Shelf ShelfCodeNavigation { get; set; } = null!;
+
+        public virtual ICollection<Author>? Authors { get; set; } = new List<Author>();
+    }
+}
