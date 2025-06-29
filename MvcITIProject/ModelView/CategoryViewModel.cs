bba8b8d653b47fa64a0daeb0b80b8ee104c842a9
Abdelkader15 <https://github.com/Abdelkader15>
2025-06-29 19:50:48 +0300

    Add Category View Model

---
+++ MvcITIProject/ModelView/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MvcITIProject.Models;
+
+namespace MvcITIProject.ModelView
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string CatName { get; set; } = null!;
+        public int BookCount { get; set; }
+
+        //public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
