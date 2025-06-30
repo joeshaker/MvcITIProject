7971ad09b30a53be000879e15eac3ee6ff7d5975
basioni <ahmedbasuony22322@gmail.com>
2025-06-30 17:00:40 +0300

    AuthorController

--- MvcITIProject/UnitOfWorks/UnitOfWork.cs
+++ MvcITIProject/UnitOfWorks/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using MvcITIProject.Models;
+﻿using MvcITIProject.IRepositories;
+using MvcITIProject.Models;
 using MvcITIProject.Repositories;
 
 namespace MvcITIProject.UnitOfWorks
@@ -6,6 +7,7 @@ namespace MvcITIProject.UnitOfWorks
     public class UnitOfWork
     {
         LibraryContext _context;
+        private readonly Dictionary<Type, object> _repositories = new();
 
         public UnitOfWork(LibraryContext context)
         {
@@ -25,8 +27,27 @@ namespace MvcITIProject.UnitOfWorks
             }
         }
 
+        private AuthorRepository _authorRepo;
 
+        public AuthorRepository authorRepo
+        {
+            get
+            {
+                if (_authorRepo == null)
+                    _authorRepo = new AuthorRepository(_context);
 
+                return _authorRepo;
+            }
+        }
+        public IGenericRepositries<T> Repository<T>() where T : class
+        {
+            if (_repositories.TryGetValue(typeof(T), out var repo))
+                return (IGenericRepositries<T>)repo;
+
+            var newRepo = new GenericRepositries<T>(_context);
+            _repositories.Add(typeof(T), newRepo);
+            return newRepo;
+        }
         public void SaveChanges()
         {
             _context.SaveChanges();
